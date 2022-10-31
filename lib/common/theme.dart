9fae874e759cefb03ebcdfea83021f2ed55f8462
Miki T <miki.tulu@gmail.com>
2022-10-31 21:17:34 +0300

    theme.dart initial commit

---
+++ lib/common/theme.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+const Color bluishClr = Color(0xFF4e5ae8);
+const Color yellowClr = Color(0xFFFFB746);
+const Color pinkClr = Color(0xFFff4667);
+const Color white = Colors.white;
+const Color primaryClr = bluishClr;
+const Color darkGreyClr = Color(0xFF121212);
+const Color darkHeaderClr = Color(0xFF424242);
+
+class Themes {
+  static final light = ThemeData(
+    primaryColor: primaryClr,
+    brightness: Brightness.light,
+  );
+
+  static final dark = ThemeData(
+    primaryColor: darkGreyClr,
+    brightness: Brightness.dark,
+  );
+}
