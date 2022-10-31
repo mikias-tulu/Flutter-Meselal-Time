b76cfb503e8ede072b98a1cc9e759652cab99e72
Miki T <miki.tulu@gmail.com>
2022-10-31 21:29:49 +0300

    initial commit
    updated theme_services.dart

--- lib/services/theme_services.dart
+++ lib/services/theme_services.dart
@@ -1,3 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:get/get.dart';
+
+class ThemeServices {
+  final _box = GetStorage();
+  final _key = 'isDarkMode';
+
+  bool _loadThemeFromBox() {
+    if (_box.read(_key)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  ThemeMode get theme => _loadThemeFromBox() ? ThemeMode.dark : ThemeMode.light;
+  void switchTheme() {
+    Get.changeThemeMode(_loadThemeFromBox() ? ThemeMode.light : ThemeMode.dark);
+  }
+}
