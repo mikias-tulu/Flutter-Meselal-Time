dff6e5f0ad6ea233ec1b0f4b5db131e82766feba
Miki T <miki.tulu@gmail.com>
2022-10-31 21:16:45 +0300

    Created home_page.dart

---
+++ lib/common/home_page.dart
@@ -0,0 +1,46 @@
+import 'package:dos/services/theme_services.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: _appBar(),
+      body: Column(
+        children: [
+          Text("Theme data"),
+        ],
+      ),
+    );
+  }
+
+  _appBar() {
+    return AppBar(
+      leading: GestureDetector(
+        onTap: () {
+          ThemeServices().switchTheme();
+        },
+        child: Icon(
+          Icons.nightlight_round,
+          size: 20,
+        ),
+      ),
+      actions: [
+        Icon(
+          Icons.person,
+          size: 20,
+        ),
+        SizedBox(
+          width: 20,
+        ),
+      ],
+    );
+  }
+}
