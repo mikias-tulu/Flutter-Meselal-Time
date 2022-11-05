024d6474f79e0c92ee5893bfd32d64bd6ba98456
Miki T <miki.tulu@gmail.com>
2022-11-05 21:42:12 +0300

    created subHeadingStyle and headingStyle functions

--- lib/common/theme.dart
+++ lib/common/theme.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 const Color bluishClr = Color(0xFF4e5ae8);
 const Color yellowClr = Color(0xFFFFB746);
@@ -10,12 +12,32 @@ const Color darkHeaderClr = Color(0xFF424242);
 
 class Themes {
   static final light = ThemeData(
+    backgroundColor: Colors.white,
     primaryColor: primaryClr,
     brightness: Brightness.light,
   );
 
   static final dark = ThemeData(
+    backgroundColor: darkGreyClr,
     primaryColor: darkGreyClr,
     brightness: Brightness.dark,
   );
+}
+
+TextStyle get subHeadingStyle {
+  return GoogleFonts.lato(
+      textStyle: TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.bold,
+    color: Get.isDarkMode ? Colors.grey[400] : Colors.grey,
+  ));
+}
+
+TextStyle get headingStyle {
+  return GoogleFonts.lato(
+      textStyle: TextStyle(
+    fontSize: 30,
+    fontWeight: FontWeight.bold,
+    color: Get.isDarkMode ? Colors.white : Colors.black,
+  ));
 }
