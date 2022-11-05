443a307a864a8a7a705723612eff0ee660b52dbf
Miki T <miki.tulu@gmail.com>
2022-11-05 21:40:56 +0300

    Add_task_bar initial commit

---
+++ lib/common/add_task_bar.dart
@@ -0,0 +1,55 @@
+import 'package:dos/common/theme.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class AddTaskPage extends StatelessWidget {
+  const AddTaskPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: context.theme.backgroundColor,
+      appBar: _appBar(context),
+      body: Container(
+        padding: const EdgeInsets.only(left: 20, right: 20),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Text(
+                "Add Task",
+                style: headingStyle,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  _appBar(BuildContext context) {
+    return AppBar(
+      elevation: 0,
+      backgroundColor: context.theme.backgroundColor,
+      leading: GestureDetector(
+        onTap: () {
+          Get.back();
+        },
+        child: Icon(
+          Icons.arrow_back_ios,
+          size: 20,
+          color: Get.isDarkMode ? Colors.white : Colors.black,
+        ),
+      ),
+      actions: [
+        CircleAvatar(
+          backgroundImage: AssetImage(
+            "Assets/images/profile.png",
+          ),
+        ),
+        SizedBox(
+          width: 20,
+        ),
+      ],
+    );
+  }
+}
