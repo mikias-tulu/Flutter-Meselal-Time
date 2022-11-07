4aa1feba323519e46c8d84511d89750f0698fe16
Miki T <miki.tulu@gmail.com>
2022-11-07 22:41:45 +0300

    Added input fields
    staeless widget changed to statefulwidget and created _getDateFromUser( ) and

--- lib/common/add_task_bar.dart
+++ lib/common/add_task_bar.dart
@@ -1,10 +1,20 @@
 import 'package:dos/common/theme.dart';
+import 'package:dos/common/widgets/input_field.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:intl/intl.dart';
 
-class AddTaskPage extends StatelessWidget {
+class AddTaskPage extends StatefulWidget {
   const AddTaskPage({super.key});
 
+  @override
+  State<AddTaskPage> createState() => _AddTaskPageState();
+}
+
+class _AddTaskPageState extends State<AddTaskPage> {
+  DateTime _selectedDate = DateTime.now();
+  String _endTime = "9:30 PM";
+  String _startTime = DateFormat("hh:mm a").format(DateTime.now()).toString();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,10 +24,58 @@ class AddTaskPage extends StatelessWidget {
         padding: const EdgeInsets.only(left: 20, right: 20),
         child: SingleChildScrollView(
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Text(
                 "Add Task",
                 style: headingStyle,
+              ),
+              MyInputField(title: "title", hint: "Enter your title"),
+              MyInputField(title: "note", hint: "Enter your note"),
+              MyInputField(
+                title: "Date",
+                hint: DateFormat.yMd().format(_selectedDate),
+                widget: IconButton(
+                    icon: Icon(
+                      Icons.calendar_today_outlined,
+                      color: Colors.grey,
+                    ),
+                    onPressed: () {
+                      _getDateFromUser();
+                    }),
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: MyInputField(
+                      title: "Start Date",
+                      hint: _startTime,
+                      widget: IconButton(
+                        onPressed: () {},
+                        icon: Icon(
+                          Icons.access_time_rounded,
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 12,
+                  ),
+                  Expanded(
+                    child: MyInputField(
+                      title: "End Date",
+                      hint: _endTime,
+                      widget: IconButton(
+                        onPressed: () {},
+                        icon: Icon(
+                          Icons.access_time_rounded,
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
@@ -51,5 +109,22 @@ class AddTaskPage extends StatelessWidget {
         ),
       ],
     );
+  }
+
+  _getDateFromUser() async {
+    DateTime? _pickerDate = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2015),
+      lastDate: DateTime(2121),
+    );
+
+    if (_pickerDate != null) {
+      setState(() {
+        _selectedDate = _pickerDate;
+      });
+    } else {
+      print("it's null or something is wrong");
+    }
   }
 }
