11d60175ef117d2baaef7f9f57496205e9b80b3c
Miki T <miki.tulu@gmail.com>
2022-11-09 23:30:59 +0300

    Create task_controller.dart

---
+++ lib/controllers/task_controller.dart
@@ -0,0 +1,28 @@
+import 'package:dos/db/db_helper.dart';
+import 'package:get/get.dart';
+
+import '../models/task.dart';
+
+class TaskController extends GetxController {
+  @override
+  void onReady() {
+    getTasks();
+    super.onReady();
+  }
+
+  var taskList = <Task>[].obs;
+
+  Future<int> addTask({Task? task}) async {
+    return await DBHelper.insert(task);
+  }
+
+  //get all the data from table
+  void getTasks() async {
+    List<Map<String, dynamic>> tasks = await DBHelper.query();
+    taskList.assignAll(tasks.map((data) => new Task.fromJson(data)).toList());
+  }
+
+  void delete(Task task) {
+    DBHelper.delete(task);
+  }
+}
