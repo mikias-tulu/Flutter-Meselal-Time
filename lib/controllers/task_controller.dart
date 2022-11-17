3db693da1ac45dfab3f67301635ce3d9725716e2
Miki T <miki.tulu@gmail.com>
2022-11-17 23:07:18 +0300

    task_controller markTaskConpleted added

--- lib/controllers/task_controller.dart
+++ lib/controllers/task_controller.dart
@@ -24,5 +24,11 @@ class TaskController extends GetxController {
 
   void delete(Task task) {
     DBHelper.delete(task);
+    getTasks();
+  }
+
+  void markTaskCompleted(int id) async {
+    await DBHelper.update(id);
+    getTasks();
   }
 }
