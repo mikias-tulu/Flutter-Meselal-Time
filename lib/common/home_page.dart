e2fe743a25a71bd86aa01c5f65552dd05db7308c
Miki T <miki.tulu@gmail.com>
2022-11-24 22:59:48 +0300

    Update home_page.dart

--- lib/common/home_page.dart
+++ lib/common/home_page.dart
@@ -28,7 +28,6 @@ class _HomePageState extends State<HomePage> {
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
     notifyHelper = NotifyHelper();
     notifyHelper.initializeNotification();
