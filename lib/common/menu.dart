ca11a59eecfbc18e24c6eee3e71cbb4417ae0f39
Miki T <miki.tulu@gmail.com>
2022-11-26 20:45:57 +0300

    Create menu.dart

---
+++ lib/common/menu.dart
@@ -0,0 +1,124 @@
+import 'package:dos/common/add_task_bar.dart';
+import 'package:dos/controllers/task_controller.dart';
+import 'package:flutter/material.dart';
+import 'package:bubble_bottom_bar/bubble_bottom_bar.dart';
+import 'package:dos/common/home_page.dart';
+import 'package:get/get.dart';
+//import 'package:flutter_native_splash/flutter_native_splash.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late int currentIndex;
+
+  @override
+  void initState() {
+    super.initState();
+    currentIndex = 0;
+  }
+
+  void changePage(int? index) {
+    setState(() {
+      currentIndex = index!;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: <Widget>[
+        HomePage(),
+        Container(
+          color: Colors.red,
+        ),
+        Container(
+          color: Colors.yellow,
+        ),
+        Container(
+          color: Colors.green,
+        ),
+      ][currentIndex],
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          await Get.to(() => const AddTaskPage());
+          Get.put(TaskController()).getTasks();
+        },
+        backgroundColor: Colors.red,
+        child: const Icon(Icons.add),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
+      bottomNavigationBar: BubbleBottomBar(
+        backgroundColor: Colors.grey[500],
+        hasNotch: true,
+        fabLocation: BubbleBottomBarFabLocation.end,
+        opacity: .2,
+        currentIndex: currentIndex,
+        onTap: changePage,
+        borderRadius: const BorderRadius.vertical(
+          top: Radius.circular(25),
+        ), //border radius doesn't work when the notch is enabled.
+        elevation: 8,
+        tilesPadding: const EdgeInsets.symmetric(
+          vertical: 8.0,
+        ),
+
+        items: const <BubbleBottomBarItem>[
+          BubbleBottomBarItem(
+            backgroundColor: Colors.red,
+            icon: Icon(
+              Icons.dashboard,
+              color: Colors.black,
+            ),
+            activeIcon: Icon(
+              Icons.dashboard,
+              color: Colors.white,
+            ),
+            title: Text(
+              "Home",
+              style: TextStyle(color: const Color(0xFFFFFFFF)),
+            ),
+          ),
+          BubbleBottomBarItem(
+              backgroundColor: Colors.deepPurple,
+              icon: Icon(
+                Icons.access_time,
+                color: Colors.black,
+              ),
+              activeIcon: Icon(
+                Icons.access_time,
+                color: Colors.deepPurple,
+              ),
+              title: Text("Logs")),
+          BubbleBottomBarItem(
+              backgroundColor: Colors.indigo,
+              icon: Icon(
+                Icons.folder_open,
+                color: Colors.black,
+              ),
+              activeIcon: Icon(
+                Icons.folder_open,
+                color: Colors.indigo,
+              ),
+              title: Text("Folders")),
+          BubbleBottomBarItem(
+              backgroundColor: Colors.green,
+              icon: Icon(
+                Icons.menu,
+                color: Colors.black,
+              ),
+              activeIcon: Icon(
+                Icons.menu,
+                color: Colors.green,
+              ),
+              title: Text("Menu"))
+        ],
+      ),
+    );
+  }
+}
