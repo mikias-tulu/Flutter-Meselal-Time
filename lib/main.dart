c82cbf7dcb0c0ad8ca00684b151b01dacebfc057
Miki T <miki.tulu@gmail.com>
2022-11-28 21:31:02 +0300

    update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,11 @@
-import 'package:dos/common/intro_page.dart';
 import 'package:dos/common/widgets/splash.dart';
 import 'package:dos/db/db_helper.dart';
 import 'package:dos/services/theme_services.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
-//import 'package:flutter_native_splash/flutter_native_splash.dart';
 
-//import 'common/add_task_bar.dart';
-import 'common/menu.dart';
 import 'common/theme.dart';
-//import 'controllers/task_controller.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
