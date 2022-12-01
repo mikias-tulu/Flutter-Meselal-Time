82818db1f07f3539baef1dd749420e8f4b7bd844
Miki T <miki.tulu@gmail.com>
2022-12-01 23:59:38 +0300

    Update progress.dart

---
+++ lib/common/pages/progress.dart
@@ -0,0 +1,20 @@
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+import 'package:pie_chart/pie_chart.dart';
+
+class ProgressPage extends StatefulWidget {
+  const ProgressPage({super.key});
+
+  @override
+  State<ProgressPage> createState() => _ProgressPageState();
+}
+
+class _ProgressPageState extends State<ProgressPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
