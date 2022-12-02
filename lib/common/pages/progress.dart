5935dacf5f19803b83d1ca8a090be6f18f05272a
Miki T <miki.tulu@gmail.com>
2022-12-02 20:39:12 +0300

    pie_chart data added

--- lib/common/pages/progress.dart
+++ lib/common/pages/progress.dart
@@ -1,20 +1,81 @@
-import 'dart:developer';
-
+import 'package:dos/db/db_helper.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:pie_chart/pie_chart.dart';
 
+import '../../controllers/task_controller.dart';
+import '../../models/task.dart';
+
 class ProgressPage extends StatefulWidget {
-  const ProgressPage({super.key});
+  ProgressPage({super.key});
 
   @override
   State<ProgressPage> createState() => _ProgressPageState();
 }
 
 class _ProgressPageState extends State<ProgressPage> {
+  final _taskController = Get.put(TaskController());
+
+  List<Color> colorList = [
+    const Color(0xffD95AF3),
+    const Color(0xff3EE094),
+    const Color(0xff3398F6),
+    const Color(0xffFA4A42),
+    const Color(0xffFE9539)
+  ];
+
+  final gradientList = <List<Color>>[
+    [
+      Color.fromRGBO(223, 250, 92, 1),
+      Color.fromRGBO(129, 250, 112, 1),
+    ],
+    [
+      Color.fromRGBO(129, 182, 205, 1),
+      Color.fromRGBO(91, 253, 199, 1),
+    ],
+    [
+      Color.fromRGBO(175, 63, 62, 1.0),
+      Color.fromRGBO(254, 154, 92, 1),
+    ]
+  ];
   @override
   Widget build(BuildContext context) {
+    Map<String, double> dataMap = {
+      "Total Tasks": Get.put(TaskController()).taskList.length.toDouble(),
+      "Completed Tasks": 1,
+      "Overdue Tasks": 4.25,
+      "Deleted": 3.51,
+    };
+
+    print(Get.put(TaskController()).taskList[0].isCompleted!.toDouble());
+
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text("Pie Chart example"),
+      ),
+      body: Center(
+        child: PieChart(
+          dataMap: dataMap,
+          colorList: colorList,
+          chartRadius: MediaQuery.of(context).size.width / 2,
+          centerText: "Progress",
+          ringStrokeWidth: 24,
+          animationDuration: const Duration(seconds: 3),
+          chartValuesOptions: const ChartValuesOptions(
+              showChartValues: true,
+              showChartValuesOutside: true,
+              showChartValuesInPercentage: true,
+              showChartValueBackground: false),
+          legendOptions: const LegendOptions(
+              showLegends: true,
+              legendShape: BoxShape.rectangle,
+              legendTextStyle: TextStyle(fontSize: 15),
+              legendPosition: LegendPosition.bottom,
+              showLegendsInRow: true),
+          gradientList: gradientList,
+        ),
+      ),
     );
   }
 }
